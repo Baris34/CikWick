d31323cb363da2d4bd3b4903dc4fdd8c0be7b76a
Baris34 <46277882+Baris34@users.noreply.github.com>
2025-07-01 23:42:22 +0300

    v0.29 Bug Fixes

--- Assets/_GameAssets/Scripts/Gameplay/Cat/CatAnimationController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Cat/CatAnimationController.cs
@@ -15,11 +15,20 @@ public class CatAnimationController : MonoBehaviour
 
     private void Update()
     {
+        if (GameManager.Instance.GetCurrentGameState() != GameState.Play &&
+            GameManager.Instance.GetCurrentGameState() != GameState.Resume && 
+            GameManager.Instance.GetCurrentGameState() != GameState.Cutscene &&
+            GameManager.Instance.GetCurrentGameState() != GameState.GameOver)
+        {
+            _animator.enabled = false;
+            return;
+        }
         SetCatAnimations();
     }
 
     private void SetCatAnimations()
     {
+        _animator.enabled = true;
         var catState = _catStateController.GetCurrentCatState();
 
         switch (catState)
--- Assets/_GameAssets/Scripts/Gameplay/Cat/CatController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Cat/CatController.cs
@@ -41,6 +41,13 @@ public class CatController : MonoBehaviour
 
     private void Update()
     {
+        if (GameManager.Instance.GetCurrentGameState() != GameState.Play &&
+            GameManager.Instance.GetCurrentGameState() != GameState.Resume &&
+            GameManager.Instance.GetCurrentGameState() != GameState.Cutscene)
+        {
+            _catAgent.speed = 0f;
+            return;
+        }
         if (_playerController.CanCatChase())
         {
             SetChaseMovement();
